2b1caaffc442c9e6e4e45035061a767a63fcc8a2
Martin Gabriel <martingabriel@martins-macbook-pro.local>
2018-06-06 20:02:57 +0200

    Initial Commit

---
+++ TSOB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TSOB
+//
+//  Created by Martin Gabriel on 06.06.18.
+//  Copyright © 2018 Martin Gabriel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
