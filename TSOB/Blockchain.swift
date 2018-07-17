c4bc6ff172410f74f23f2242dcc8407129f4b67c
Martin Gabriel <martingabriel@martins-macbook-pro.local>
2018-07-17 21:28:16 +0200

    First version: UI with blockchain picker, data download, sound creation, sound player

---
+++ TSOB/Blockchain.swift
@@ -0,0 +1,88 @@
+//
+//  Blockchain.swift
+//  TSOB
+//
+//  Created by Martin Gabriel on 14.06.18.
+//  Copyright © 2018 Martin Gabriel. All rights reserved.
+//
+
+import Foundation
+
+class blockchain {
+    var Name = ""
+    var ID = 0
+    var Url = ""
+    
+    init(name: String, id: Int, url: String) {
+        self.Name = name
+        self.ID = id
+        self.Url = url
+    }
+    
+    func getAudioFileFromUrl() -> Data? {
+        do {
+            let dataFromUrl = try Data(contentsOf: URL(string: self.Url)!)
+            let sound = createSound(data: dataFromUrl)
+            let data = sound as Data
+            return data
+        } catch let error as NSError {
+            print(error)
+            return nil
+        }
+    }
+    
+    func createSound(data: Data) -> NSMutableData {
+        let sampleRate: Int32 = 44100
+        let chunkSize: Int32 = 36
+        let subChunkSize: Int32 = 16
+        let format: Int16 = 1
+        let channels: Int16 = 1
+        let bitsPerSample: Int16 = 16
+        let byteRate: Int32 = sampleRate * Int32(channels * bitsPerSample / 8)
+        let blockAlign: Int16 = channels * 2
+        let dataSize: Int32 = Int32(data.count)
+        
+        let header = NSMutableData()
+        
+        header.append([UInt8]("RIFF".utf8), length: 4)
+        header.append(intToByteArray(chunkSize), length: 4)
+        
+        //WAVE
+        header.append([UInt8]("WAVE".utf8), length: 4)
+        
+        //FMT
+        header.append([UInt8]("fmt ".utf8), length: 4)
+        
+        header.append(intToByteArray(subChunkSize), length: 4)
+        header.append(shortToByteArray(format), length: 2)
+        header.append(shortToByteArray(channels), length: 2)
+        header.append(intToByteArray(sampleRate), length: 4)
+        header.append(intToByteArray(byteRate), length: 4)
+        header.append(shortToByteArray(blockAlign), length: 2)
+        header.append(shortToByteArray(bitsPerSample), length: 2)
+        
+        header.append([UInt8]("data".utf8), length: 4)
+        header.append(intToByteArray(dataSize), length: 4)
+        header.append(data)
+        
+        return header
+    }
+    
+    func intToByteArray(_ i: Int32) -> [UInt8] {
+        return [
+            //little endian
+            UInt8(truncating: ((i     ) & 0xff) as NSNumber),
+            UInt8(truncating: ((i >>  8) & 0xff) as NSNumber),
+            UInt8(truncating: ((i >> 16) & 0xff) as NSNumber),
+            UInt8(truncating: ((i >> 24) & 0xff) as NSNumber)
+        ]
+    }
+    
+    func shortToByteArray(_ i: Int16) -> [UInt8] {
+        return [
+            //little endian
+            UInt8(truncating: ((i      ) & 0xff) as NSNumber),
+            UInt8(truncating: ((i >>  8) & 0xff) as NSNumber)
+        ]
+    }
+}
--- TSOB/ViewController.swift
+++ TSOB/ViewController.swift
@@ -7,19 +7,135 @@
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
+    var pickerDataSource = [blockchain]()
+    var audioPlayer: AVAudioPlayer!
+    var isPlaying: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.pickerView.dataSource = self
+        self.pickerView.delegate = self
+        
+        // init data source
+        pickerDataSource.append(blockchain(name: "BTC block", id: 4, url: "https://webbtc.com/block/000000000000000001f942eb4bfa0aeccb6a14c268f4c72d5fff17270da771b9.hex"))
+        
+        // defaults
+        self.playButton.isHidden = false
+        self.stopButton.isHidden = true
+        self.loading.isHidden = true
+        
+        // init player and init first sound
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            let firstSound = pickerDataSource[0]
+            prepareAudioFromSelectedBlockchain(block: firstSound)
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let string = pickerDataSource[row].Name
+        return NSAttributedString(string: string, attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
     }
 
+    @IBAction func playTouch(_ sender: Any) {
+        if (!isPlaying) {
+            audioPlayer.play()
+            isPlaying = true
+            playButton.isHidden = true
+            stopButton.isHidden = false
+        }
+    }
+    
+    @IBAction func stopTouch(_ sender: Any) {
+        if (isPlaying) {
+            audioPlayer.stop()
+            isPlaying = false
+            playButton.isHidden = false
+            stopButton.isHidden = true
+        }
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        let volumeSlider = (MPVolumeView().subviews.filter { NSStringFromClass($0.classForCoder) == "MPVolumeSlider" }.first as! UISlider)
+        volumeSlider.setValue(slider.value, animated: false)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return pickerDataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataSource[row].Name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let block = pickerDataSource[pickerView.selectedRow(inComponent: 0)]
+        prepareAudioFromSelectedBlockchain(block: block)
+    }
+    
+    
+    func prepareAudioFromSelectedBlockchain(block: blockchain)
+    {
+        let queue = DispatchQueue(label: "DownloadQueue")
+        
+        queue.async {
+            do {
+                // animate loading
+                DispatchQueue.main.async {
+                    self.animateLoading(active: true)
+                }
+                
+                // download data and prepare audioplayer
+                let sound = block.getAudioFileFromUrl()
+                self.audioPlayer = try AVAudioPlayer(data: sound!)
+                self.audioPlayer.prepareToPlay()
+                
+                // stop animating loading
+                DispatchQueue.main.async {
+                    self.animateLoading(active: false)
+                }
+                
+                // player active
+                if (self.isPlaying) {
+                    self.audioPlayer.play()
+                }
+            } catch let error as NSError {
+                print(error.debugDescription)
+                print(block.Name)
+            }
+        }
+    }
+    
+    func animateLoading(active state: Bool) {
+        self.playButton.isUserInteractionEnabled = !state
+        self.loading.isHidden = !state
+        
+        // animate
+        state ? self.loading.startAnimating() : self.loading.stopAnimating()
+    }
 }
+
 
